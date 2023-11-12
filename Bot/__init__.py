75028b1a860e0ee746557f4231103d72d6a6c527
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-11-12 09:39:55 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -2,18 +2,28 @@ import os
 import sys
 import logging
 from pyrogram import Client
-from config import *
 
 if os.path.exists('error.log'):
     os.remove('error.log')
 
+# <-----------LOGGING------------>
 logging.basicConfig(level=logging.INFO, filename='error.log')
 LOG = logging.getLogger("Bot by @soheru")
 LOG.setLevel(level=logging.INFO)
-
+# <-----------Variables-------------->
 LOG.info('❤️ Checking Bot Variables.....')
-
-#<---------------Connecting-------------->
+TRIGGERS = os.environ.get("TRIGGERS", "/ !").split(" ")
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '6312969509:AAGAk_Qj_vS7spkPQnLFY9ohqN1ieO2NYLk')  # BOT Token Add
+API_ID = int(os.environ.get('API_ID', 3847632))  # Telgram Api id
+APP_HASH = os.environ.get('APP_HASH', '1a9708f807ddd06b10337f2091c67657')  # Telgram App hash
+OWNER_ID = int(os.environ.get('OWNER_ID', 6440253535))
+MONGO_DB = os.environ.get("MONGO_DB", 'mongodb+srv://whocarestobi:whocarestobi@cluster0.ayo8gc4.mongodb.net/?retryWrites=true&w=majority')  # MONGO DB FOR ANIME DATA
+FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1001940830839)  # Must Fill This, Add Bot As Admin In Log Channel
+BOT_NAME = os.environ.get('BOT_NAME', 'Anime fusion 2nd')
+# <-----------Variables For 4GB Support (Optional)-------------->
+SESSION_STRING = os.environ.get("SESSION_STRING", 'None')  # Replace None With String Session Your String Session Account Must Be Present In Log Channel
+ubot = None  # Don't Touch This
+# <---------------Connecting-------------->
 if BOT_TOKEN is not None:
     try:
         encoder = Client('AutoEncoder', api_id=API_ID, api_hash=APP_HASH, bot_token=BOT_TOKEN, plugins=dict(root="Bot/plugins"))
@@ -21,17 +31,16 @@ if BOT_TOKEN is not None:
     except Exception as e:
         LOG.warn(f'😞 Error While Connecting To Bot\nCheck Errors: {e}')
         sys.exit()
-
-#<---------------4GB Connecting-------------->
+# <---------------4GB Connecting-------------->
 def create_ubot():
     global SESSION_STRING
     global ubot
     if SESSION_STRING != "None":
         try:
-            ubot = Client("AutoEncoder", session_string=SESSION_STRING, api_id=API_ID, api_hash=APP_HASH, plugins=plugins)
+            ubot = Client("AutoEncoder", session_string=SESSION_STRING, api_id=API_ID, api_hash=API_HASH, plugins=plugins)
             LOGS.info("❤️ 4GB String Session Connected")
             return ubot
         except:
-            LOGS.info('😞 Error While Connecting To String Session')    
-            sys.exit()   
+            LOGS.info('😞 Error While Connecting To String Session')
+            sys.exit()
             return None
