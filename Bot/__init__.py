2d3d714cbb7b11b867abde23831a19c26b1576f1
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-11-12 09:51:41 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -16,7 +16,7 @@ TRIGGERS = os.environ.get("TRIGGERS", "/ !").split(" ")
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '6979551576:AAE35Fp2ui6XXyJ629EEN_ZXlpgaZsZgqzY')  # BOT Token Add
 API_ID = int(os.environ.get('API_ID', 3847632))  # Telgram Api id
 APP_HASH = os.environ.get('APP_HASH', '1a9708f807ddd06b10337f2091c67657')  # Telgram App hash
-OWNER_ID = int(os.environ.get('OWNER_ID', 6440253535))
+OWNER_ID = int(os.environ.get('OWNER_ID', 6748415360))
 MONGO_DB = os.environ.get("MONGO_DB", 'mongodb+srv://whocarestobi:whocarestobi@cluster0.ayo8gc4.mongodb.net/?retryWrites=true&w=majority')  # MONGO DB FOR ANIME DATA
 FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1001940830839)  # Must Fill This, Add Bot As Admin In Log Channel
 BOT_NAME = os.environ.get('BOT_NAME', 'fucking encoding bot')
