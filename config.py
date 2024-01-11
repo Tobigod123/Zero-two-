5567711b77902723bf2a51c7d6a632a79041ab57
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2024-01-11 13:06:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 TRIGGERS = os.environ.get("TRIGGERS", "/ !").split(" ")
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '6312969509:AAGAk_Qj_vS7spkPQnLFY9ohqN1ieO2NYLk')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '6915492272:AAFPlDwVTVy5UoHhGvOL3K4x5Ts6uNB8Jvw')
 API_ID = int(os.environ.get('API_ID', 3847632))
 APP_HASH = os.environ.get('APP_HASH', '1a9708f807ddd06b10337f2091c67657')
 OWNER_ID = int(os.environ.get('OWNER_ID', 6748415360))
